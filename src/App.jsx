e827315507b9c6f2471e9068b1127e5d772a2905
shaikwasims1 <shaikw720@gmail.com>
2026-03-07 00:45:09 +0530

    Fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,23 @@
 import "./App.css"
-import {BrowserRouter as Router ,Routes,Route} from "react-router-dom"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+
 import Navbar from "./components/Navbar"
 import Home from "./components/pages/home/Home"
 import Movies from "./components/pages/movies/Movies"
-import Horror from "./components/pages/horror/Horror"
 import NotFound from "./components/pages/notFound/NotFound"
-
+import Horror from "./components/pages/Horror/Horror"
 
 const App = () => {
   return (
-    <>
-      <Router>
-        <Navbar/>
-        <Routes>
-          <Route path="/" element={<Home/>}/>
-          <Route path="/movies" element={<Movies/>}/>
-          <Route path="/Horror" element={<Horror/>}/>
-          <Route path="*" element={<NotFound/>}/>
-        </Routes>
-      </Router>
-    </>
+    <Router>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/movies" element={<Movies/>}/>
+        <Route path="/horror" element={<Horror/>}/>
+        <Route path="*" element={<NotFound/>}/>
+      </Routes>
+    </Router>
   )
 }
 
